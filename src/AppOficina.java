acbecb1cc8d41debcaa4a2ac167704c13beb3b80
Leonardo de Souza Galvão <160603231+LeonardodeSouzaGalvao@users.noreply.github.com>
2025-04-05 09:19:12 -0300

    Feat: Implementação dos algoritmos MergeSort e Seleção

--- src/AppOficina.java
+++ src/AppOficina.java
@@ -88,7 +88,9 @@ public class AppOficina {
     static int exibirMenuOrdenadores() {
         cabecalho();
         System.out.println("1 - Bolha");
-        System.out.println("2 - Inserção");     
+        System.out.println("2 - Inserção");   
+        System.out.println("3 - MergeSort");  
+        System.out.println("4 - Seleção");
         System.out.println("0 - Finalizar");
        
         return lerNumero("Digite sua opção", Integer.class);
@@ -173,6 +175,8 @@ public class AppOficina {
         switch (opcao) {
             case 1 -> ordenador = new Bubblesort<>();
             case 2 -> ordenador = new InsertSort<>();
+            case 3 -> ordenador = new Mergesort<>();
+            case 4 -> ordenador = new Seleção<>();
         }
 
         switch (ordenacao) {
